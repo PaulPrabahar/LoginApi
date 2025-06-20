3440abaf524a2742625b614e97ee97e7818a50ec
Paul Prabahar <paul.prabahar@aravind.org>
2025-06-20 13:36:33 +0530

    Authentication jwt settings completed.

---
+++ Login/Login.Api/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    public LoginController()
+    {
+    }
+
+    //Create or Register User Profile
+    [HttpPost]
+    public Task<IActionResult> Register()
+    {
+
+    }
+}
